81a788d1d5511c4a8c998c501d01af789fcfae9d
DeMing Zhu <dmzhu@mdl.com>
2016-06-24 17:34:37 +0800

    no message

---
+++ MKTagsViewController/MKTagsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKTagsViewController.h
+//  
+//
+//  Created by Mingenesis on 16/6/24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKTagsViewController : UIViewController
+
+@end
---
+++ MKTagsViewController/MKTagsViewController.m
@@ -0,0 +1,21 @@
+//
+//  MKTagsViewController.m
+//  
+//
+//  Created by Mingenesis on 16/6/24.
+//
+//
+
+#import "MKTagsViewController.h"
+
+@interface MKTagsViewController ()
+
+@end
+
+@implementation MKTagsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
