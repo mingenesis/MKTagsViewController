9b466603fa9c5b79caef652be55d1768e7f87812
DeMing Zhu <dmzhu@mdl.com>
2016-06-29 18:07:34 +0800

    no message

--- Example/TagsViewController/HomeViewController.m
+++ Example/TagsViewController/HomeViewController.m
@@ -10,9 +10,10 @@
 #import "MKTagsViewController.h"
 #import "TopicViewController.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<MKTagsViewControllerDelegate>
 
 @property (nonatomic, weak) MKTagsViewController *tagsViewController;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *tagButtons;
 
 @end
 
@@ -30,19 +31,36 @@
     TopicViewController *vc3 = [[TopicViewController alloc] init];
     vc3.title = @"vc3";
     
+    self.tagsViewController.delegate = self;
     self.tagsViewController.viewControllers = @[vc1, vc2, vc3];
+    
+    [self updateTagButtons];
+}
+
+- (void)updateTagButtons {
+    for (UIButton *button in self.tagButtons) {
+        button.selected = self.tagsViewController.viewControllers[button.tag] == self.tagsViewController.selectedViewController;
+    }
 }
 
 - (IBAction)tagButtonDidClick:(UIButton *)sender {
     UIViewController *vc = self.tagsViewController.viewControllers[sender.tag];
     
     self.tagsViewController.selectedViewController = vc;
+    
+    [self updateTagButtons];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"Body"]) {
         self.tagsViewController = segue.destinationViewController;
     }
+}
+
+#pragma mark - MKTagsViewControllerDelegate
+
+- (void)tagsViewController:(MKTagsViewController *)tagsViewController didSelectViewController:(UIViewController *)viewController {
+    [self updateTagButtons];
 }
 
 @end
